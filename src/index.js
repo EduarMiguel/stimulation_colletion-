39c4727b2aaebd1d295ce925ea752bd55f3720fe
EduarMiguel <miguelmercado9388@gmail.com>
2019-05-16 20:57:17 +0000

    Cambios a la ruta de origen

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   let users = [
-    { name: "Jhon", lastName: "Doe", age: 30, Genero:"Masculino"},
-    { name: "María", lastName: "Sharápova", age: 32, Genero:"Femenino"},
-    {name: "Miguel", lastname: "Mercado", age: 26, Genero:"Masculino"}
+    { idUsuario: 01, name: "Luis", lastName: "Perez", age: 30, Genero:"Masculino", telefono: 7732112},
+    { idUsuario: 02, name: "María", lastName: "Lopez", age: 32, Genero:"Femenino", telefono: 7442434},
+    { idUsuario: 03, name: "Miguel", lastname: "Mercado", age: 26, Genero:"Masculino", telefono: 7731221}
      ];
   res.send(users);
 });
@@ -21,6 +21,24 @@ app.get("/progreso", (req, res) => {
     {idProgreso: 003, status: "Medio", fechainicio: 15/05, fechafinal:22/05}
      ];
   res.send(progreso);
+});
+
+app.get("/nivel", (req,res) => {
+    let nivel = [
+        {idNivel: 05, Nombre: "NivelOne", Status: "aceptable"},
+        {idNivel: 06, Nombre: "NivelTwo", Status: "insuficiente"},
+        {idNivel: 07, Nombre: "NivelOne", Status: "excelente"}
+    ];
+    res.send(nivel);
+});
+
+app.get("/actividades", (req,res) => {
+    let actividades = [
+        {idActividades: 011, Nombre: "Actividad1"},
+        {idActividades: 012, Nombre: "Actividad2"},
+        {idActividades: 013, Nombre: "Actividad2"}
+    ];
+    res.send(actividades);
 });
 
 app.listen(3000, () => {
